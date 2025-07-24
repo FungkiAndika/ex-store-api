31482da1983adbe0f3825bdebf904fea4689e5ff
Fungki <92091580+FungkiAndika@users.noreply.github.com>
2025-07-25 05:53:31 +0700

    .

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
   return res.json({
